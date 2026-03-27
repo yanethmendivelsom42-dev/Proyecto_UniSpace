5ea1621451c75014dea9223488a7d1c39d214911
yanethmendivelsom42-dev <yanethmendivelsom42@gmail.com>
2026-03-27 15:00:50 +0000

    Corregir startup.sh para Azure

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PORT=${PORT:-8000}
 cd backend
-gunicorn -w 2 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:$PORT
+gunicorn -w 2 -k uvicorn.workers.UvicornWorker main:app --bind=0.0.0.0:$PORT
